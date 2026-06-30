582d1afbb5ae4c7c8271fe14a5face5a3b9e9957
meto10002-arch <meto10002@gmail.com>
2026-06-30 04:54:19 +0300

    Implement chat functionality with Firebase integration

--- script.js
+++ script.js
@@ -1,3 +1,13 @@
+import {
+db,
+collection,
+addDoc,
+query,
+orderBy,
+onSnapshot,
+serverTimestamp
+} from "./firebase.js";
+
 const login=document.getElementById("login");
 const chat=document.getElementById("chat");
 
@@ -6,6 +16,20 @@ const preview=document.getElementById("preview");
 
 const nameInput=document.getElementById("name");
 const loginBtn=document.getElementById("loginBtn");
+
+const messages=document.getElementById("messages");
+const text=document.getElementById("text");
+const send=document.getElementById("send");
+
+let myName=localStorage.getItem("forever_name");
+let myPhoto=localStorage.getItem("forever_photo");
+
+if(myName && myPhoto){
+
+login.style.display="none";
+chat.style.display="flex";
+
+}
 
 photo.onchange=()=>{
 
@@ -18,6 +42,8 @@ const reader=new FileReader();
 reader.onload=(e)=>{
 
 preview.src=e.target.result;
+
+myPhoto=e.target.result;
 
 };
 
@@ -35,7 +61,7 @@ return;
 
 }
 
-if(photo.files.length==0){
+if(!myPhoto){
 
 alert("اختر صورة");
 
@@ -43,8 +69,92 @@ return;
 
 }
 
+myName=nameInput.value;
+
+localStorage.setItem("forever_name",myName);
+
+localStorage.setItem("forever_photo",myPhoto);
+
 login.style.display="none";
 
 chat.style.display="flex";
 
 };
+const q = query(
+  collection(db, "private_chat"),
+  orderBy("time")
+);
+
+send.onclick = async () => {
+
+  if (text.value.trim() == "") return;
+
+  await addDoc(
+    collection(db, "private_chat"),
+    {
+      sender: myName,
+      photo: myPhoto,
+      text: text.value,
+      time: serverTimestamp()
+    }
+  );
+
+  text.value = "";
+
+};
+onSnapshot(q,(snapshot)=>{
+
+messages.innerHTML="";
+
+snapshot.forEach((doc)=>{
+
+const data=doc.data();
+
+const msg=document.createElement("div");
+
+msg.className="message";
+
+if(data.sender===myName){
+
+msg.classList.add("me");
+
+}else{
+
+msg.classList.add("friend");
+
+}
+
+let t="";
+
+if(data.time){
+
+t=new Date(data.time.seconds*1000).toLocaleTimeString("ar-IQ",{
+hour:"2-digit",
+minute:"2-digit"
+});
+
+}
+
+msg.innerHTML=`
+
+<img class="avatar" src="${data.photo}">
+
+<div class="bubble">
+
+<div class="sender">${data.sender}</div>
+
+<div class="text">${data.text}</div>
+
+<div class="time">${t}</div>
+
+</div>
+
+`;
+
+messages.appendChild(msg);
+
+});
+
+messages.scrollTop=messages.scrollHeight;
+
+});
